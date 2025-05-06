3f4d17a95a88c2b8b85a45a9a116210d490fdf05
PhuAnh69 <phanphuanh6969@gmail.com>
2025-05-06 17:05:55 +0700

    add .gitignore

--- Backend/obj/Debug/net8.0/Final-year-Project.AssemblyInfo.cs
+++ Backend/obj/Debug/net8.0/Final-year-Project.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Final-year-Project")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8e88c02810d2a51c3d8064a60c40f4aaf87a17fa")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fd0a6ebcdcabd8022489e34a365a6be27c1ee671")]
 [assembly: System.Reflection.AssemblyProductAttribute("Final-year-Project")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Final-year-Project")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
